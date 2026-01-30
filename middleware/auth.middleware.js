c56f7f3488ba7f313f47947170a4d291213ce489
Smeet Hendre <smeethendre4@gmail.com>
2026-01-30 23:18:27 +0530

    Refactor verifyUser middleware to handle token extraction and user verification

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,9 +1,26 @@
 import User from "../model/user.model.js";
+import { ApiError } from "../util/apiError.util.js";
+import jwt from "jsonwebtoken";
+import asyncHandler from "../util/asyncHandler.util.js";
 
-const verifyUser = async(User) => {
-    const user = await User.findById(this._id);
+const verifyUser = asyncHandler(async (req, res, next) => {
+  const token =
+    req.cookie?.accessToken ||
+    req.header("Authorization")?.replace("Bearer ", "");
 
-    if(!user){
-        
-    }
-}
+  if (!token) throw new ApiError(401, "Unauthorized access");
+
+  const decodedToken = jwt.verify(token, process.env.accessToken);
+
+  const user = await User.findById(decodedToken._id).select(
+    "-password -refreshToken",
+  );
+
+  if (!user) throw new ApiError(401, "User doesn't exist");
+
+  req.user = user;
+
+  next();
+});
+
+export { verifyUser };
